5c5c9d3372e91d9d578cfa923560c64ffa0a41b4
Tiffany Tay <tiffany.tay@gmail.com>
2024-09-29 17:51:17 -0500

    updated testpython

--- testpython.py
+++ testpython.py
@@ -1,10 +1,11 @@
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+df = pd.read_excel(r"Sales Dataset.xlsx")
+df.head() #see dataset sample
 
-df = pd.read_excel(r"/Sales Dataset.xlsx")
-df.head()
+df.info()
+
+df.describe()
