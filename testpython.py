e0bd4e3239830e89c49b458ed95d40a360664c82
Tiffany Tay <tiffany.tay@gmail.com>
2024-09-29 15:02:44 -0500

    updated inex and testpython

--- testpython.py
+++ testpython.py
@@ -1,12 +1,10 @@
 
-'''python
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-'''
 
-'''python
+
 df = pd.read_excel(r"/Sales Dataset.xlsx")
 df.head()
-'''
